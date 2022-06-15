00e1a17e588d1cc6e60f72b47816e507449ff8e2
ytakegawa <ytkgw1986@gmail.com>
2022-06-15 13:55:01 +0900

    Add: Todo_app parts (HTML, JS, CSS)
    Todoアプリの構成を追加

---
+++ todo_app/js/main.js
@@ -0,0 +1,48 @@
+(function () {
+  'use strict'
+
+  let vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: [
+      {
+        title: 'task 1',
+        isDone: false
+      },
+      {
+        title: 'task 2',
+        isDone: false
+      },
+      {
+        title: 'task 3',
+        isDone: true
+      }
+      ]
+    },
+    methods: {
+      addItem: function(){
+        let item = {
+          title: this.newItem,
+          isDone: false
+        }
+        this.todos.push(item)
+        this.newItem=''
+      },
+      deleteItem: function(index){
+        if (confirm('are you sure?')){
+          this.todos.splice(index, 1)
+        }
+      }
+    },
+    computed: {
+      remaining: function() {
+        let items = this.todos.filter(function(todo) {
+          return !todo.isDone;
+        });
+        return items.length;
+      }
+    }
+  })
+})()
+
