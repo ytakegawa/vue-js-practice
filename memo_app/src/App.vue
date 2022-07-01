0146038f1ad6df05bf7d2dbc443cb1602d071796
ytakegawa <ytkgw1986@gmail.com>
2022-07-01 16:39:20 +0900

    Add: Memo show, NewMemo add
    メモ一覧と新規メモ追加機能追加

--- memo_app/src/App.vue
+++ memo_app/src/App.vue
@@ -1,15 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>MY MEMO APP</h1>
+  <button @click="toggleNewMemoModal">ADD MEMO</button>
+  <ul>
+    <li v-for="memo in memos" :key="memo.title">
+      <span >{{ memo.title }}</span>
+      <span @click="toggleMemoDetailModal" class="command" name="memo.title">[DETAIL]</span>
+      <!-- <div v-if="showMemoDetailModal">
+      <MemoDetailModal>
+        <h1>MEMO DETAIL</h1>
+        <p>SHOW? EDIT? DELETE?</p>
+        <p>TITLE</p>
+        <input type="text" v-model="momo.title" >
+        <p>CONTENT</p>
+        <textarea v-model="momo.body">{{ memo.body }}</textarea>
+        <br>
+        <button @click.prevent="editMemo()">EDIT DONE</button>
+      </MemoDetailModal>
+      </div> -->
+    </li>
+  </ul>
+  <div v-if="showNewMemoModal">
+      <NewMemoModal>
+        <h1>ADD NEW MEMO</h1>
+        <p>INPUT MEMO DETAIL</p>
+        <p>TITLE</p>
+        <input type="text" v-model="newTitle">
+        <p>CONTENT</p>
+        <textarea v-model="newBody"></textarea>
+        <br>
+        <button @click.prevent="addMemo()">FINISH</button>
+      </NewMemoModal>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewMemoModal from './components/NewMemoModal.vue'
+import MemoDetailModal from './components/MemoDetailModal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { NewMemoModal, MemoDetailModal },
+  data() {
+    return {
+      newTitle: '',
+      newBody: '',
+      memos: [],
+      showNewMemoModal: false,
+      showMemoDetailModal: false
+    }
+  },
+  watch: {
+    memos: {
+      handler: function () {
+        localStorage.setItem('memos', JSON.stringify(this.memos))
+      },
+      deep: true
+    }
+  },
+  mounted: function () {
+    this.memos = JSON.parse(localStorage.getItem('memos')) || []
+  },
+  methods: {
+    toggleNewMemoModal(){
+      this.showNewMemoModal = !this.showNewMemoModal
+    },
+    toggleMemoDetailModal(){
+      this.showMemoDetailModal = !this.showMemoDetailModal
+    },
+    addMemo: function () {
+        const memo = {
+          title: this.newTitle,
+          body: this.newBody,
+
+        }
+        this.memos.push(memo)
+        this.newTitle = ''
+        this.newBody = ''
+      },
   }
 }
 </script>
@@ -23,4 +91,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#app ul {
+  font-size: 20px;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  margin-bottom: 10px;
+}
+
+.command {
+  cursor: pointer;
+}
+
 </style>
---
+++ memo_app/src/components/MemoDetailModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="backdrops" >
+    <div class="modal" >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal(){
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+
+  .backdrops {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+
+  .modal h1 {
+    color:black;
+  }
+
+
+</style>
